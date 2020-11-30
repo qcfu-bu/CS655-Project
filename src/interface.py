a1382c6c810a1b45c5f9a1015dff6f22974f5f71
chantisnake <zcsx.ok@outlook.com>
2020-11-29 19:56:27 -0500

    accommodate the separation of send and receive.

--- src/interface.py
+++ src/interface.py
@@ -5,23 +5,32 @@ import socket
 from typing import Optional
 
 from addresses import Address
-from socket_util import receive_data_from, send_message
+from socket_util import receive_msg_from, send_msg_to
 from worker_interface_message import gen_message, parse_worker_task_num_msg, \
     WorkerTaskNumMsg, NewTaskToWorkerMsg
 
 
 def get_worker_task_num(worker_address: Address) -> Optional[int]:
+    """Get a task num from a worker
 
+    If the worker no longer accepts task, return None
+    :param worker_address: the address of the worker
+    :return: If the worker is accepting task,
+        return the current task that the worker has,
+        else None
+    """
     # generate the message to pin the worker
     new_task_msg = NewTaskToWorkerMsg()
 
+    # TODO: timeout?
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
         # connecting with the worker
         conn.connect((worker_address.ip, worker_address.port))
         print("connected to host", (worker_address.ip, worker_address.port))
 
         # send the message and parse the result
-        task_num_msg_str = send_message(conn, gen_message(new_task_msg))
+        send_msg_to(conn, gen_message(new_task_msg))
+        task_num_msg_str = receive_msg_from(conn)
         task_num_msg = parse_worker_task_num_msg(task_num_msg_str)
 
         # return the result
