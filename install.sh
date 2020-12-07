1eac904d821c68eff13785d4c5d3d0cbd871cfa6
Qiancheng Fu <qcfu@bu.edu>
2020-12-07 18:42:36 -0500

    fixed caching error

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ sudo apt install -y python3-dev python3-pip
 yes | pip3 install --upgrade pip
 yes | pip3 install --upgrade numpy
 yes | pip3 install --upgrade pillow
-yes | pip3 install --upgrade tensorflow
+yes | pip3 install --upgrade --no-cache-dir tensorflow
 yes | pip3 install --upgrade Flask
 pip3 install -e .
