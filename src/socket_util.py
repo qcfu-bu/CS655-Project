1b1202a707bba20809645c738d975562590251e1
chantisnake <zcsx.ok@outlook.com>
2020-12-07 11:18:18 -0500

    added logging to socket_util.py

--- src/socket_util.py
+++ src/socket_util.py
@@ -1,6 +1,8 @@
 # General Utility for socket uses
 import socket
 
+from src.logging_util import Logger
+
 K = 1024
 
 TIMEOUT_TIME = 10  # seconds
@@ -9,6 +11,8 @@ DATA_CAP = 1 * K  # data to send in a single packet
 ENCODING = 'utf-8'  # use utf-8 for text encoding
 
 MSG_ENDING_CHAR = b'\n'  # this char signifies the ending of a message
+
+LOGGER = Logger(logging_keyword="SOCKET")
 
 
 def receive_msg_from(connection: socket.socket) -> str:
@@ -19,15 +23,20 @@ def receive_msg_from(connection: socket.socket) -> str:
     """
     # setting the timeout time
     connection.settimeout(TIMEOUT_TIME)
+    LOGGER.debug(f"Setting timeout time to {TIMEOUT_TIME}")
 
+    LOGGER.info(f"receiving message from connection")
     total_recv = b""
     while True:
         cur_recv: bytes = connection.recv(DATA_CAP)
+        LOGGER.debug(f"received part of the message: {cur_recv}")
         if cur_recv.endswith(MSG_ENDING_CHAR):
             total_recv = total_recv + cur_recv
             break
         else:
             total_recv = total_recv + cur_recv
+
+    LOGGER.info(f"total message Received: {total_recv.decode(ENCODING)}")
 
     return total_recv.decode(ENCODING)
 
@@ -40,6 +49,7 @@ def send_msg_to(connection: socket.socket, msg: str) -> None:
     """
     # setting the timeout time
     connection.settimeout(TIMEOUT_TIME)
+    LOGGER.debug(f"Setting timeout time to {TIMEOUT_TIME}")
 
     connection.sendall(msg.encode(ENCODING) + MSG_ENDING_CHAR)
 
@@ -52,10 +62,16 @@ def send_file_to(connection: socket.socket, send_file_name: str) -> None:
     """
     # setting the timeout time
     connection.settimeout(TIMEOUT_TIME)
+    LOGGER.debug(f"Setting timeout time to {TIMEOUT_TIME}")
+
+    LOGGER.info(f"Starting to send file {send_file_name}")
 
     with open(send_file_name, 'rb') as f:
         file_content = b'\n'.join(f.readlines())
+        LOGGER.debug(f"total file content:\n{file_content}")
         connection.sendall(file_content)
+
+    LOGGER.info(f"finish sending file {send_file_name}")
 
 
 def receive_file_from(connection: socket.socket, save_file_name: str) -> None:
@@ -66,6 +82,7 @@ def receive_file_from(connection: socket.socket, save_file_name: str) -> None:
     """
     # setting the timeout time
     connection.settimeout(TIMEOUT_TIME)
+    LOGGER.debug(f"Setting timeout time to {TIMEOUT_TIME}")
 
     with open(save_file_name, 'wb') as f:
 
@@ -73,6 +90,8 @@ def receive_file_from(connection: socket.socket, save_file_name: str) -> None:
         # to receive.
         while True:
             data_recv = connection.recv(DATA_CAP)
+            LOGGER.debug(f"received part of the message: {data_recv}")
+
             if len(data_recv) < DATA_CAP:
                 break
 
